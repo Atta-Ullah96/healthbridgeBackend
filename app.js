f016fbf81dcab6690b605dfea7e20db2f504bc38
Attaullah <attaullah@gmail.com>
2026-04-02 17:14:06 +0500

    just removing the console from the both files

--- app.js
+++ app.js
@@ -13,18 +13,13 @@ import { spawn } from 'child_process';
 // import { generateSlots } from './utils/slotGenerator.js';
 
 const isProduction = NODE_ENV === 'production';
-console.log("current env" ,NODE_ENV);
 
-console.log("isProduction" , isProduction);
-
-console.log("both origin before array" ,PROD_ADMIN_HEALTHBRIDGE_DOMAIN, PROD_HEALTHBIRDGE_DOMAIN );
 
 
 const allowedOrigins = isProduction
 ? [PROD_HEALTHBIRDGE_DOMAIN, PROD_ADMIN_HEALTHBRIDGE_DOMAIN]
 : [DEV_ORIGIN_1, DEV_ORIGIN_2];
 
-console.log("origins" ,allowedOrigins);
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin) return callback(null, true); // allow Postman, mobile apps
